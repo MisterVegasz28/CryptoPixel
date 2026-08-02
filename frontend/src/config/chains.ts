22e25d449bdeec09a15628b3fd41a278fe878f25
MisterVegasz28 <leo.delaitre90@gmail.com>
2026-08-02 03:13:16 +0200

    ca dégage

--- frontend/src/config/chains.ts
+++
@@ -1,6 +0,0 @@
-import { addRpcUrlOverrideToChain } from '@privy-io/react-auth';
-import { polygonAmoy } from 'viem/chains';
-
-const INDEXER_URL = import.meta.env.VITE_INDEXER_URL;
-
-export const polygonAmoyOverride = addRpcUrlOverrideToChain(polygonAmoy, `${INDEXER_URL}/rpc`);
